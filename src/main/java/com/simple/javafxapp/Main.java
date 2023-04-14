3183358c68fd68c72726e61d2fa9c824cc9652a9
Nathan <admin@corndogit.dev>
2023-04-14 12:14:44 +0100

    add option to create new grid thing game from main menu

--- src/main/java/com/simple/javafxapp/Main.java
+++ src/main/java/com/simple/javafxapp/Main.java
@@ -8,11 +8,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+    static final int WINDOW_WIDTH = 800;
+    static final int WINDOW_HEIGHT = 800;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("grid-thing-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Awesome program 2");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), WINDOW_WIDTH, WINDOW_HEIGHT);
+        stage.setTitle("Awesome program");
         stage.setScene(scene);
         stage.show();
     }
